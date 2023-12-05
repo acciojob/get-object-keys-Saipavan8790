2fca0ab9a890d370209291ac2d48856d60edf2c7
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-12-05 18:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const student = {
+	name = chhkuli;
+}
+
+// Add getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  // Get all keys of the object
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+const keysArray = student.getKeys();
+
+// Print the result
+console.log(keysArray); // Output: chhkuli
